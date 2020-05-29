f8f22b277872613e0e412b2ee398eea7acaa6a8a
Alireza <cheraghalialireza33@gmail.com>
2020-05-29 17:55:20 +0430

    Add Audio Of Post And Add Audio Type Complete (MySQL)

--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,13 @@
 import TypeOfPost from "../component/SocialMedia/TypeOfPost";
-
+import ReactAudioPlayer from 'react-audio-player';
+//...
 function test() {
     return(<div>
-        <TypeOfPost/>
+        <ReactAudioPlayer
+            src={"./static/Audio/audioFile-2020-05-29,5_49_58.mp3"}
+            autoPlay
+            controls
+        />
     </div>)
 }
 export default test
